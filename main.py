0c780db8f1103e7fccfe43275fb8f9b681e72d6d
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-05-15 19:22:21 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import matplotlib
+matplotlib.use('TkAgg')
+
+
+import matplotlib.pyplot as plt
+from pylsl import StreamInlet, resolve_byprop
+
+# ストリームの情報を取得
+streams = resolve_byprop('type', 'ACC', timeout=2)
+if len(streams) == 0:
+    print("ストリームが見つかりませんでした。")
+    exit()
+# ストリームを選択
+inlet = StreamInlet(streams[0])
+
+# データを取得し、グラフで表示する
+plt.ion()  # インタラクティブモードをオンにする
+fig, ax = plt.subplots(1, 1)
+x = []
+y = []
+while True:
+    # データを取得
+    sample, timestamp = inlet.pull_sample()
+    # データが欠損している場合はスキップ
+    if sample is None:
+        continue
+    # データをプロット
+    x.append(timestamp)
+    y.append(sample)
+    ax.clear()
+    ax.plot(x, y)
+    plt.pause(0.001)
