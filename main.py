b906acd6abf43b7406e108e632e7db90175bd93f
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-05-17 08:56:13 +0900

    add: quit in win

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ matplotlib.use('TkAgg')
 
 import matplotlib.pyplot as plt
 from pylsl import StreamInlet, resolve_byprop
+import sys
+import select
 
 # ストリームの情報を取得
 streams = resolve_byprop('type', 'ACC', timeout=2)
@@ -18,7 +20,27 @@ plt.ion()  # インタラクティブモードをオンにする
 fig, ax = plt.subplots(1, 1)
 x = []
 y = []
-while True:
+running = True  # グラフの継続フラグ
+
+def stop_graph():
+    global running
+    running = False
+
+# キーボード入力を非同期で監視し、'q'が押されたらループを終了する
+def keyboard_input():
+    while running:
+        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            key = sys.stdin.readline().strip()
+            if key == 'q':
+                stop_graph()
+
+# キーボード入力を非同期で監視するための設定
+import threading
+input_thread = threading.Thread(target=keyboard_input)
+input_thread.daemon = True
+input_thread.start()
+
+while running:
     # データを取得
     sample, timestamp = inlet.pull_sample()
     # データが欠損している場合はスキップ
@@ -30,3 +52,6 @@ while True:
     ax.clear()
     ax.plot(x, y)
     plt.pause(0.001)
+
+# グラフが停止した後、キーボード入力のスレッドを終了する
+input_thread.join()
