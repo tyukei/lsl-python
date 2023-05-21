493c8b33e6f668b76b5203d401ab39f7ecd29fcc
tyukei <0024481294bxyk6037@gmail.com>
2023-05-21 17:50:24 +0900

    wip

--- main-mac.py
+++ main-mac.py
@@ -1,72 +1,37 @@
-import matplotlib
-matplotlib.use('MacOSX')
-
-import matplotlib.pyplot as plt
-from pylsl import StreamInlet, resolve_byprop
-import sys
-import select
 import time
+import tkinter as tk
+import turtle
 
-# ストリームの情報を取得
-streams = resolve_byprop('type', 'ACC', timeout=2)
-if len(streams) == 0:
-    print("ストリームが見つかりませんでした。")
-    exit()
-else:
-    # stream information
-    print(streams[0].name())
-# ストリームを選択
-inlet = StreamInlet(streams[0])
+# ウィンドウを作成
+root = tk.Tk()
+root.title("Real-time Graph")
+canvas = tk.Canvas(root, width=800, height=600)
+canvas.pack()
 
-# データを取得し、グラフで表示する
-plt.ion()  # インタラクティブモードをオンにする
-fig, ax = plt.subplots(1, 1)
-x = []
-y = []
-running = True  # グラフの継続フラグ
+# Turtleを作成
+t = turtle.RawTurtle(canvas)
 
-def stop_graph():
-    global running
-    running = False
+# グラフの描画設定
+t.pensize(2)
+t.speed(0)
+t.up()
+t.goto(-380, -280)
+t.down()
 
-# キーボード入力を非同期で監視し、'q'が押されたらループを終了する
-def keyboard_input():
-    while running:
-        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-            key = sys.stdin.readline().strip()
-            if key == 'q':
-                stop_graph()
-
-# キーボード入力を非同期で監視するための設定
-import threading
-input_thread = threading.Thread(target=keyboard_input)
-input_thread.daemon = True
-input_thread.start()
-
-min_y = float('inf')
-max_y = float('-inf')
-
-while running:
-    start_time = time.time()  # 開始時間を記録
+# グラフを描画
+for i in range(1000):
     # データを取得
     sample, timestamp = inlet.pull_sample()
     # データが欠損している場合はスキップ
     if sample is None:
         continue
+    x, y, z = sample
+    
     # データをプロット
-    x.append(timestamp)
-    y.append(sample)
-    min_y = min(min_y, *y)
-    max_y = max(max_y, *y)
-    ax.clear()
-    ax.plot(x, y)
-    ax.set_xlim(timestamp - 10, timestamp)  # 横軸の範囲を設定
-    ax.set_ylim(min_y, max_y)  # 縦軸の範囲を設定
-    plt.pause(0.0001)
-    elapsed_time = time.time() - start_time  # データ取得にかかった時間
-    sleep_time = 0.0001 - elapsed_time
-    if sleep_time > 0:
-        time.sleep(sleep_time)
+    t.goto(i - 380, x * 100)
+    t.goto(i - 380, y * 100)
+    t.goto(i - 380, z * 100)
 
-# グラフが停止した後、キーボード入力のスレッドを終了する
-input_thread.join()
+    time.sleep(0.01)  # グラフ更新のための待機時間
+
+root.mainloop()
--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ python3 -m venv .venv
 DIR=$(realpath $(dirname $0))
 PATH=$DIR/.venv/bin/activate
 source $PATH
+python3 -m pip install --upgrade pip
 pip install matplotlib
 pip install pylsl
+python3 main-mac.py
 deactivate
