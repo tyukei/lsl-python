da2138d6ba4804e4fa500a3908a4d28b5ccdbeed
tyukei <0024481294bxyk6037@gmail.com>
2023-06-01 08:47:41 +0900

    add: acc

--- acc.py
+++ acc.py
@@ -5,12 +5,13 @@ from pylsl import resolve_byprop, StreamInlet
 # Initialization function
 def init():
     ax.set_xlim(xlim[0],xlim[1]) # x軸固定
+    ax.set_ylim(-20000, 20000) # y軸固定
     line1, = ax.plot([], [], label='X', color='red')
     line2, = ax.plot([], [], label='Y', color='blue')
     line3, = ax.plot([], [], label='Z', color='green')
     ax.set_title("ACC") # タイトルを初期化
     ax.set_xticks([]) # 横軸の目盛りを削除
-    ax.set_yticks([]) # 縦軸の目盛りを削除
+    # ax.set_yticks([]) # 縦軸の目盛りを削除
     ax.set_xlabel("Time 10sec")
     ax.legend(bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0, fontsize=8)
     return line1, line2, line3, 
@@ -37,8 +38,8 @@ def animate(i):
     line1.set_data(xdata, y1data)
     line2.set_data(xdata, y2data)
     line3.set_data(xdata, y3data)
-    ax.relim()
-    ax.autoscale_view()
+    # ax.relim()
+    # ax.autoscale_view()
     return line1, line2, line3,
 
 def main():
